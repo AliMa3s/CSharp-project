7c2e292fc03cb130698351cb6baa22fac12b8aa5
Ali-Maes <79624543+AliMa3s@users.noreply.github.com>
2021-06-16 10:46:52 +0200

    Add files via upload

---
+++ Rederij/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using scheepvaart;
+using System;
+
+namespace ConsoleApp1 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            
+            Ship s1 = new CargoShip(120, 50, "Ali's Cargo", 600000);
+            Ship s2 = new RoroShip(54, 350, "RoroRarara", 7500004, 35, 8, Lading.Diesel);
+            Ship s3 = new CruiseShip(87, 68, "NangaParbat", 5000, Lading.nafta);
+            Ship s4 = new Containership(85, 45, "Going Merry", 487321548, 98630, Lading.Amoniak);
+            Console.WriteLine(s1 + Lading.LNG.ToString());
+            Console.WriteLine(s2 + Lading.Diesel.ToString());
+            Console.WriteLine(s3 + Lading.nafta.ToString());
+            Console.WriteLine(s4 + Lading.Amoniak.ToString());
+
+            
+        }
+        
+       
+    }
+    }
