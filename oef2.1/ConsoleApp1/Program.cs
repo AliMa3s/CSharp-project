acc14ed91fa0a03edeb88bb98b4e4235438ff015
Ali-Maes <79624543+AliMa3s@users.noreply.github.com>
2021-06-16 10:34:57 +0200

    Add files via upload

---
+++ oef2.1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using oef2._1;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Vervoer v = new Vervoer("lotter");
+            Console.WriteLine(v);
+            v.Lawai();
+            v.drinkt();
+            Console.WriteLine("--------------------");
+            Auto a = new Auto("auto");
+            Console.WriteLine(a);
+            a.Lawai();
+            a.drinkt();
+            Console.WriteLine("--------------------");
+            Fiets f = new Fiets("fiets");
+            Console.WriteLine(f);
+            f.Lawai();
+            f.drinkt();
+            List<Vervoer> vervoers = new List<Vervoer>();
+            vervoers.Add(v);
+            vervoers.Add(a);
+            vervoers.Add(f);
+            foreach(Vervoer ver in vervoers) {
+                Console.WriteLine(ver);
+                ver.Lawai();
+                ver.drinkt();
+                Console.WriteLine("-------------------");
+            }
+        }
+    }
+}
