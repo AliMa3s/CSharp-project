52322b3a1c16a1d3c666e14d98cdbc626e3c8052
Ali-Maes <79624543+AliMa3s@users.noreply.github.com>
2021-06-16 10:54:08 +0200

    Add files via upload

---
+++ OpdrachtFileIOv2/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Verwerking;
+
+namespace ConsoleApp1 {
+    class Program {
+        static void Main(string[] args) {
+            AdresData r = new AdresData();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            r.UitZip();
+            r.Verwerk();
+            
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Verwijder?");
+            Console.ReadLine();
+            r.Verwijder();
+            Console.ResetColor();
+        }
+    }
+}
