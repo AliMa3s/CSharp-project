29d779cc1e657591c0491ed61a139504fc954338
Ali-Maes <79624543+AliMa3s@users.noreply.github.com>
2021-06-16 10:44:55 +0200

    Add files via upload

---
+++ ProjectGps0.1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using DataReader;
+using System;
+
+namespace ConsoleApp1 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            //string path = @"C:\Users\alima\source\repos\ProjectGPS1\WRdata";
+            //Processor p = new Processor();
+            //p.LeesBestanden(path, "WRstraatnamen.csv","WRdata.csv");
+            //var straten = p.GeefStraten();
+            //Console.WriteLine(straten.Count);
+            string path = @"C:\Users\alima\source\repos\ProjectGPS1\WRdata";
+            Processor pr = new Processor();
+            pr.LeesBestanden(path, "WRstraatnamen.csv", "WRdata.csv");
+            DatabaseInit di = new DatabaseInit(@"Data Source=desktop-ps4v4ia\sqlexpress;Initial Catalog=wegen;Integrated Security=True");
+            di.WriteStraat(pr.Straten);
+        }
+    }
+}
