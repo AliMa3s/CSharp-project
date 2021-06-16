412eb110e26d5f52527fef1181f07174cb11e91c
Ali-Maes <79624543+AliMa3s@users.noreply.github.com>
2021-06-16 09:45:11 +0200

    Add files via upload

---
+++ AirLine/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using Maatschappij;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Airline airline = new Airline();
+            Finance finance = new Finance();
+            Sales sales = new Sales();
+            Catering catering = new Catering();
+
+            //link modules with events
+            airline.FlightEvent += finance.OnFlightEvent;
+            airline.FlightEvent += sales.OnFlightEvent;
+            airline.FlightEvent += catering.OnFlightEvent;
+            catering.CateringEvent += finance.OnCateringEvent;
+
+            //simulate app
+            FlightReader fr = new FlightReader();
+            List<Flight> flights = fr.ReadFlights();
+            foreach (var f in flights) {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(f);
+                airline.AddFlight(f);
+            }
+
+            //check observers
+            catering.PrintOrderReport();
+            sales.PrintAnalysisReport();
+            finance.PrintCateringReport(2021);
+            finance.PrintFuelReport(2021);
+        }
+    }
+}
