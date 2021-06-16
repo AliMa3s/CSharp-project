b93a628296aca4bc7cb68302f5cc027d1aa9ff44
Ali-Maes <79624543+AliMa3s@users.noreply.github.com>
2021-06-16 10:58:14 +0200

    Add files via upload

---
+++ OpdrachtWinkelEvent/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using WinkelEvents;
+
+namespace ConsoleApp1 {
+    class Program {
+        static void Main(string[] args) {
+            Winkel w = new Winkel();
+            Sales s = new Sales();
+            Stockbeheer sb = new Stockbeheer();
+            Groothandelaar gh = new Groothandelaar();
+
+            sb.ToonStock();
+            w.WinkelVerkoop += s.OnWinkelVerkoop;
+            
+            
+            sb.StockWijziging += gh.OnStockWijziging;
+            w.WinkelVerkoop += sb.OnWinkelVerkoop;
+            
+            
+            //sb.StockWijziging += gh.OnStockWijziging;
+           
+
+            // Verkoop van winkelproducten
+            Console.WriteLine("Verkoop van producten...");
+            w.VerkoopProduct(new Bestelling(ProductType.Dubbel, 3.99, 35, "Dorpstraat 5, Lievegem"));
+            w.VerkoopProduct(new Bestelling(ProductType.Kriek, 2.99, 25, "Dorpstraat 5, Lievegem"));
+            w.VerkoopProduct(new Bestelling(ProductType.Dubbel, 3.99, 35, "Kerkstraat 155, Zele"));
+            w.VerkoopProduct(new Bestelling(ProductType.Kriek, 2.99, 55, "Dorpstraat 5, Lievegem"));
+            
+            
+      
+        }
+    }
+}
