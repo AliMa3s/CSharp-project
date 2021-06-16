23ebceb12ac5f890d2dd9991674c7c645cfd6d87
Ali-Maes <79624543+AliMa3s@users.noreply.github.com>
2021-06-16 10:45:56 +0200

    Add files via upload

---
+++ Rederij/ConsoleApp1/Fleet.cs
@@ -0,0 +1,14 @@
+﻿using scheepvaart;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1 {
+    public class Fleet : Ship {
+        public Dictionary<String, Ship> _ships = new Dictionary<string, Ship>();
+
+        public Fleet(string name) {
+            Name = name;
+        }
+    }
+}
