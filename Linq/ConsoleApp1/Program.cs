617982eb1acdc5bc8ac18988ad79e346f5fd5f8d
Ali-Maes <79624543+AliMa3s@users.noreply.github.com>
2021-06-16 10:24:45 +0200

    Add files via upload

---
+++ Linq/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using LinqVirusDataAnalyzerLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApp1 {
+    class Program {
+        static void Main(string[] args) {
+            string path = @"C:\Users\alima\Downloads\COVID19BE_muni\COVID19BE_muni_cum.csv";
+            DataAnalyzer da = new DataAnalyzer();
+            
+            //List<DataMunicipalityCumulative> dataMc = DataReader.ReadCumlativeMunicipalityData(Path.Combine(path, "COVID19BE_muni_cum.csv"),false);
+            List<DataMunicipalityCumulative> dataMc = DataReader.ReadCumlativeMunicipalityData(path, false);
+            da.SetDataMunicipalityCumulative(dataMc);
+            foreach (var m in da.GetMunicipalities("Antwerpen")) {
+                Console.WriteLine(m);
+            }
+
+        }
+    }
+}
