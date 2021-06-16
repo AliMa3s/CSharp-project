69971d80cbb46fa9b6c1298bda8766ad736f5f3e
Ali-Maes <79624543+AliMa3s@users.noreply.github.com>
2021-06-16 10:37:06 +0200

    Add files via upload

---
+++ PartyTime/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using PartyOef;
+
+namespace ConsoleApp1 {
+    class Program {
+        static void Main(string[] args) {
+            List<Party> parties = new List<Party>();
+            Console.WriteLine("Hello World!");
+            DinnerParty d1 = new DinnerParty(5, false,false);
+            DinnerParty d2 = new DinnerParty(15, true, true);
+            DinnerParty d3 = new DinnerParty(10, true, false);
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+            d1.AantalMensen = 10;
+            Console.WriteLine(d1);
+            BirthdayParty b1 = new BirthdayParty(6, true, "52");
+            BirthdayParty b2 = new BirthdayParty(5, false, "Hello");
+            Console.WriteLine(b1);
+            parties.Add(d1);parties.Add(d2);parties.Add(d3);parties.Add(b1);parties.Add(b2);
+            foreach (Party p in parties) {
+                Console.WriteLine($"{p}, {p.Kost}");
+            }
+        }
+    }
+}
