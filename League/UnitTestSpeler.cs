c667ac895b3d30f950b973d514f0d502955c58a4
Ali-Maes <79624543+AliMa3s@users.noreply.github.com>
2021-06-16 10:23:00 +0200

    Add files via upload

---
+++ League/UnitTestSpeler.cs
@@ -0,0 +1,58 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XunitsTestTeamsManager {
+   public class UnitTestSpeler {
+        [Theory]
+        [InlineData(10,"naam","Testnaam","Testnaam")]
+        [InlineData(10,"naam","Testnaam    ","Testnaam")]
+        public void Test_ZetNaam_valid(int id, string naam, string newNaam, string resNaam) {
+            Speler s = new Speler(id, naam);
+            s.ZetNaam(newNaam);
+            Assert.Equal(resNaam, s.Naam);
+        }
+        [Theory]
+        [InlineData(10,"naam", " ")]
+        [InlineData(10,"naam", " \n    ")]
+        [InlineData(10,"naam", null)]
+        public void Test_ZetNaaminvalid(int id, string naam, string newNaam) {
+            Speler s = new Speler(id, naam);
+            Assert.Throws<SpelerException>(() => s.ZetNaam(newNaam));
+            Assert.Equal(naam, s.Naam);
+        }
+
+        [Fact]
+        public void Test_Zetnaam() {
+            Speler s = new Speler(10, "naam");
+            Team t = new Team(1 ,"Team1");
+            s.ZetTeam(t);
+            Assert.Equal(t, s.Team);
+            Assert.Contains(s, t.Spelers());
+            Assert.True(t.HeeftSpeler(s));
+
+        }
+        [Theory]
+        [InlineData(99)]
+        [InlineData(1)]
+
+        public void ZetRugnummer_Rugnummer(int rugnummer) {
+            Speler s = new Speler(11, "Ali");
+            s.ZetRugnummer(rugnummer);
+            Assert.Equal(rugnummer, s.Rugnummer);
+        }
+        [Theory]
+        [InlineData (11, "AliA",0)]
+        [InlineData (11, "AliA",100)]
+        [InlineData (11, "AliA",-2)]
+        public void ZetRugnummer_invalid(int id, string spelernaam, int nieuweRugnummer) {
+            Speler s = new Speler(id, spelernaam);
+            s.ZetRugnummer(90);
+            var ex = Assert.Throws<SpelerException>(() => s.ZetRugnummer(nieuweRugnummer));
+            Assert.Equal("ZetRugnummer", ex.Message);
+            Assert.Equal(90, s.Rugnummer);
+        }
+    }
+}
---
+++ League/UnitTestTeam.cs
@@ -0,0 +1,69 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XunitsTestTeamsManager {
+   public class UnitTestTeam {
+        private Team team;
+        private List<Speler> spelers = new List<Speler>();
+        public UnitTestTeam() {
+            team = new Team(10, "TestTeamNaam");
+            spelers.Add(new Speler(5, "Jeff"));
+            spelers.Add(new Speler(6, "Cis"));
+            spelers.Add(new Speler(9, "Jules"));
+        }
+
+        [Theory]
+        [InlineData(10, "naam", "Testnaam", "Testnaam")]
+        [InlineData(10, "naam", " Testnaam    ", "Testnaam")]
+        public void Test_ZetNaam_valid(int id, string naam, string newNaam, string resNaam) {
+            Team t = new Team(id, naam);
+            t.ZetNaam(newNaam);
+            Assert.Equal(resNaam, t.Naam);
+        }
+        [Theory]
+        [InlineData(5, "Jeff")]
+        [InlineData(9, "Jules")]
+        public void Test_HeeftSpeler_valid(int id, string naam) {
+            foreach (Speler s in spelers) team.VoegSpelerToe(s);
+            Speler sp = new Speler(id, naam);
+            Assert.True(team.HeeftSpeler(sp));
+        }
+        [Theory]
+        [InlineData(6, "Jeff")]
+        [InlineData(5, "Jules")]
+        public void Test_HeeftSpeler_invalid(int id, string naam) {
+            Assert.False(team.HeeftSpeler(new Speler(id, naam)));
+        }
+        [Fact]
+        public void Test_VoegSpelerToe_valid() {
+            team.VoegSpelerToe(spelers[0]);
+            Assert.True(team.HeeftSpeler(spelers[0]));
+            Assert.Equal(1, team.Spelers().Count);
+            Assert.Equal(team, spelers[0].Team);
+
+            team.VoegSpelerToe(spelers[1]);
+            Assert.True(team.HeeftSpeler(spelers[0]));
+            Assert.True(team.HeeftSpeler(spelers[1]));
+            Assert.Equal(2, team.Spelers().Count);
+            Assert.Equal(team, spelers[0].Team);
+            Assert.Equal(team, spelers[1].Team);
+        }
+        [Fact]
+        public void Test_VoegSpelerToe_invalid() {
+            team.VoegSpelerToe(spelers[0]);
+            Assert.True(team.HeeftSpeler(spelers[0]));
+            Assert.Equal(1, team.Spelers().Count);
+            Assert.Equal(team, spelers[0].Team);
+
+            Assert.Throws<TeamException>(() => team.VoegSpelerToe(spelers[0]));
+            Assert.Throws<TeamException>(() => team.VoegSpelerToe(null));
+
+            Assert.True(team.HeeftSpeler(spelers[0]));
+            Assert.Equal(1, team.Spelers().Count);
+            Assert.Equal(team, spelers[0].Team);
+        }
+    }
+}
